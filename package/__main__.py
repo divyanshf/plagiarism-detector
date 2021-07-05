5494d7ca9dcbaf3d35866f8eb8d308ac81c115fa
DivyanshFalodiya <divyanshfalodiyaab@gmail.com>
2021-07-05 18:09:28 +0530

    :art: added screen processor for modularity

--- package/__main__.py
+++ package/__main__.py
@@ -2,8 +2,8 @@ import os
 from typing import Optional
 import numpy as np
 import typer
-import datetime
 import pandas as pd
+from .ScreenProcessor.ScreenProcessor import ScreenProcessor
 from .Analyser import PathAnalyser, Preference
 from .IREProcessor import IREProcessor
 from .Processor.FileProcessor import featureMatrix
@@ -18,6 +18,10 @@ userpref = pref.initialize()
 # Typer app
 app = typer.Typer(help='Plagiarism Detection in source code files.')
 app.add_typer(prefapp, name='preference', help='Customize preferences')
+
+
+# Screen Processor
+sp = ScreenProcessor(userpref=userpref)
 
 
 # Process corpus
@@ -47,89 +51,26 @@ def calculateSimilarity(files, pcomment):
     corpusCode = [doc.file for doc in files]
     corpusComment = [doc.commentsStr for doc in files]
 
+    initial = sp.printProcessInitial('Calculating textual similarity...')
     simCode = processCorpus(corpusCode, 'normal')
     simCode = simCode * 100
+    sp.printProcessFinal(initial, 'Textual similarity calculated!')
 
     if pcomment:
-        commentsWeight = float(userpref['comments_weight'])  # In percentage
-        simComment = processCorpus(corpusComment, filenames, 'idf')
+        initial = sp.printProcessInitial('Calculating comments similarity...')
+        commentsWeight = float(userpref['comment_weight'])  # In percentage
+        simComment = processCorpus(corpusComment, 'idf')
         simComment = simComment * 100
         simCode = ((100 - commentsWeight) * simCode +
                    commentsWeight * simComment) / 100
+        sp.printProcessFinal(initial, 'Comments Similarity calculated!')
 
-    # simFeatures = processFeatures(files) * 100
-    # simCode = (simCode + simFeatures) / 2
+    initial = sp.printProcessInitial('Calculating feature-based similarity...')
+    simFeatures = processFeatures(files) * 100
+    simCode = (simCode + simFeatures) / 2
+    sp.printProcessFinal(initial, 'Feature-based Similarity calculated!')
 
     return simCode
-
-
-# Save results to a csv file
-def saveResults(dataframe, paths):
-    timestamp = datetime.datetime.now()
-    date = timestamp.strftime("%Y-%m-%d")
-    time = timestamp.strftime("%I-%M-%S %p")
-    basepath = userpref['result_path'] + '\\' + date + '\\' + time
-    try:
-        if not(os.path.exists(basepath)):
-            os.makedirs(basepath)
-        # os.chmod(path, stat.S_IRWXO)
-        path = basepath + '\\' + 'result.csv'
-        dataframe.to_csv(path)
-        with open(basepath + '\\readme.txt', 'w') as file:
-            text = ''
-            for path in paths:
-                if path:
-                    text = text + 'Input : ' + os.path.abspath(path) + '\n'
-            text = text + 'Preferences used:\n'
-            for key, value in userpref.items():
-                text = text + key + ' : ' + value + '\n'
-            file.write(text)
-            file.close()
-        result = 'Saved to ' + basepath
-        typer.secho(result, fg=typer.colors.GREEN)
-    except Exception as ex:
-        typer.secho(str(ex), fg=typer.colors.RED)
-        raise typer.Exit()
-
-
-# Represent max two sims
-def representBinary(sims, files, paths):
-    threshold = float(userpref['threshold'])
-    simCode = np.triu(sims)
-    filenames = [file.filename for file in files]
-    bestIndices = np.argwhere(simCode >= threshold)
-    bestIndices = list(filter(lambda x: x[0] != x[1], bestIndices))
-    result = np.zeros(len(bestIndices))
-    columns = []
-    for index, value in enumerate(bestIndices):
-        col = filenames[value[0]] + ' and ' + filenames[value[1]]
-        columns.append(col)
-        result[index] = simCode[value[0]][value[1]]
-    if len(bestIndices) > 0:
-        df = pd.DataFrame(result.transpose(), index=columns,
-                          columns=['Similarity'])
-        df = df.sort_values(by=['Similarity'], ascending=False)
-        saveResults(df, paths)
-    else:
-        typer.secho('NO PLAGIARISM ABOVE THRESHOLD FOUND.',
-                    fg=typer.colors.GREEN)
-
-
-# Represent multiple file sims
-def representPrimary(simCode, files, paths):
-    threshold = float(userpref['threshold'])
-    filenames = [file.filename for file in files]
-    filenames[0] = 'PRIMARY'
-    columns = []
-    simCode = simCode[0, :]
-    bestIndices = np.argwhere(simCode >= threshold)
-    result = np.zeros(len(bestIndices))
-    for index, value in enumerate(bestIndices):
-        result[index] = simCode[value[0]]
-        columns.append(filenames[value[0]])
-    df = pd.DataFrame(result, columns=['Similarity'], index=[columns])
-    df = df.sort_values(by=['Similarity'], ascending=False)
-    saveResults(df, paths)
 
 
 # Detect similarity
@@ -151,18 +92,13 @@ def compare(path1: str = typer.Argument(..., help='Path to a file or folder'), p
             files = analyser.processPath(path1)
             # Notify user
             if len(files) > 0:
-                text = 'Processing...\r'
-                typer.secho(text, fg=typer.colors.YELLOW, nl=False)
+                initial = sp.printProcessInitial('Processing files...')
             # Process features
             for file in files:
                 file.processDocument()
             # Notify user
             if len(files) > 0:
-                lText = len(text)
-                empString = (' ' * lText) + '\r'
-                typer.echo(empString, nl=False)
-                text = 'Processed!'
-                typer.secho(text, fg=typer.colors.GREEN)
+                sp.printProcessFinal(initial, 'Files Processed!')
         else:
             text = path1 + ' : ' + error
             typer.secho(text, fg=typer.colors.RED)
@@ -174,18 +110,13 @@ def compare(path1: str = typer.Argument(..., help='Path to a file or folder'), p
             files = analyser.processPath(path1) + analyser.processPath(path2)
             # Notify user
             if len(files) > 0:
-                text = 'Processing...\r'
-                typer.secho(text, fg=typer.colors.YELLOW, nl=False)
+                initial = sp.printProcessInitial('Processing files...')
             # Process features
             for file in files:
                 file.processDocument()
             # Notify user
             if len(files) > 0:
-                lText = len(text)
-                empString = (' ' * lText) + '\r'
-                typer.echo(empString, nl=False)
-                text = 'Processed!'
-                typer.secho(text, fg=typer.colors.GREEN)
+                sp.printProcessFinal(initial, 'Files Processed!')
             isDir2, error = analyser.isDir(path2)
             if isDir2:
                 rep = 'p'
@@ -200,9 +131,9 @@ def compare(path1: str = typer.Argument(..., help='Path to a file or folder'), p
     if len(files) != 0:
         result = calculateSimilarity(files, pcomment)
         if rep == 'b':
-            representBinary(result, files, [path1, path2])
+            sp.representBinary(result, files, [path1, path2])
         else:
-            representPrimary(result, files, [path1, path2])
+            sp.representPrimary(result, files, [path1, path2])
     else:
         text = 'No .' + filetype + ' files found!'
         typer.secho(text, fg=typer.colors.RED)
@@ -226,18 +157,13 @@ def extract(path: str = typer.Argument(..., help='Path to the file or folder')):
             files = analyser.processPath(path)
             # Notify user
             if len(files) > 0:
-                text = f'{file.filename} : Processing...\r'
-                typer.secho(text, fg=typer.colors.YELLOW, nl=False)
+                initial = sp.printProcessInitial('Processing files...')
             for file in files:
                 # Extract features
                 file.extractFeatures()
             if len(files) != 0:
                 # Notify user
-                lText = len(text)
-                empString = (' ' * lText) + '\r'
-                typer.echo(empString, nl=False)
-                text = f'{file.filename} : Processed!'
-                typer.secho(text, fg=typer.colors.GREEN)
+                sp.printProcessFinal(initial, 'Files Processed!')
 
                 # Get feature matrix
                 result, features = featureMatrix(files)
